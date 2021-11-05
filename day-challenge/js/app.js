accd7b8f410314731d9ea72bf4fae60eb9f4ca9f
FranklinTOliveira <38389844+FranklinTOliveira@users.noreply.github.com>
2021-11-04 18:18:50 -0700

    semicolons

--- day-challenge/js/app.js
+++ day-challenge/js/app.js
@@ -1,12 +1,12 @@
 const getMoviesBySearchTerm = async (searchTerm) => {
 	const url = new URL("http://www.ombdapi.com");
-	url.searchParams.append("apikey", a94b905c)
+	url.searchParams.append("apikey", a94b905c);
 	url.searchParams.append("s", searchTerm);
 	return await fetch(url);
 }
 const getMovieDetailsById = async (id) => {
 	const url = new URL("http://www.ombdapi.com");
-	url.searchParams.append("apikey", a94b905c)
+	url.searchParams.append("apikey", a94b905c);
 	url.searchParass.append("i", id);
 	return await fetch(url);
 }
